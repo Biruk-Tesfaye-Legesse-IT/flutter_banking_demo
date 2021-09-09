f971f50722e9785c469168491a453cb44aae0d75
Biruk Tesfaye Legesse <it.biruk.tesfaye2913@gmail.com>
2021-09-09 05:16:58 +0300

    Sep 9 5 AM

---
+++ lib/application/bloc/CommissionBloc/commission_event.dart
@@ -0,0 +1,13 @@
+part of 'commission_bloc.dart';
+
+abstract class CommissionEvent extends Equatable {
+  const CommissionEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class RequestPayment extends CommissionEvent {
+  @override
+  List<Object> get props => [];
+}
